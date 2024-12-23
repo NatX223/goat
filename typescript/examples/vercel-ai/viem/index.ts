df40f059246143837a8a0e67d855331604ace715
Ajuzie Sinachi <Ajuziesinachi@gmail.com>
2024-12-23 11:39:48 +0100

    feat: Testing the token-launcher plugin.

--- typescript/examples/vercel-ai/viem/index.ts
+++ typescript/examples/vercel-ai/viem/index.ts
@@ -1,13 +1,16 @@
-import { openai } from "@ai-sdk/openai";
+// import { openai } from "@ai-sdk/openai";
+import readline from "node:readline";
+import { xai } from "@ai-sdk/xai";
 import { generateText } from "ai";
 
 import { http } from "viem";
 import { createWalletClient } from "viem";
 import { privateKeyToAccount } from "viem/accounts";
-import { sepolia } from "viem/chains";
+import { sepolia, modeTestnet } from "viem/chains";
 
 import { getOnChainTools } from "@goat-sdk/adapter-vercel-ai";
 import { PEPE, USDC, erc20 } from "@goat-sdk/plugin-erc20";
+import { tokenLauncher } from "@goat-sdk/plugin-token-launcher";
 
 import { sendETH } from "@goat-sdk/wallet-evm";
 import { viem } from "@goat-sdk/wallet-viem";
@@ -19,21 +22,51 @@ const account = privateKeyToAccount(process.env.WALLET_PRIVATE_KEY as `0x${strin
 const walletClient = createWalletClient({
     account: account,
     transport: http(process.env.RPC_PROVIDER_URL),
-    chain: sepolia,
+    chain: modeTestnet,
 });
 
 (async () => {
     const tools = await getOnChainTools({
         wallet: viem(walletClient),
-        plugins: [sendETH(), erc20({ tokens: [USDC, PEPE] })],
+        plugins: [sendETH(), erc20({ tokens: [USDC, PEPE] }), tokenLauncher()],
     });
 
-    const result = await generateText({
-        model: openai("gpt-4o-mini"),
-        tools: tools,
-        maxSteps: 5,
-        prompt: "Get the balance of the USDC token",
+    const rl = readline.createInterface({
+        input: process.stdin,
+        output: process.stdout,
     });
 
-    console.log(result.text);
+    while (true) {
+        const prompt = await new Promise<string>((resolve) => {
+            rl.question('Enter your prompt (or "exit" to quit): ', resolve);
+        });
+
+        if (prompt === "exit") {
+            rl.close();
+            break;
+        }
+
+        console.log("\n-------------------\n");
+        console.log("TOOLS CALLED");
+        console.log("\n-------------------\n");
+
+        console.log("\n-------------------\n");
+        console.log("RESPONSE");
+        console.log("\n-------------------\n");
+        try {
+            const result = await generateText({
+                model: xai("grok-2-1212"),
+                tools: tools,
+                maxSteps: 10,
+                prompt: prompt,
+                onStepFinish: (event) => {
+                    console.log(event.toolResults);
+                },
+            });
+            console.log(result.text);
+        } catch (error) {
+            console.error(error);
+        }
+        console.log("\n-------------------\n");
+    }
 })();
